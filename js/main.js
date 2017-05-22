4d974b5e0470d4d8d459627ab5448d42817e5a93
DuarteMF <duartepferreira@hotmail.com>
2017-05-22 10:27:22 +0100

    Adding search and edit functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var AddButtonClickedOn = false;
+var EditButtonClickedOn = false;
+
 $("li a").click(function(){
 	$activeTab = $(this).parent();
 	$allTabs = $("li","ul");
@@ -14,9 +17,34 @@ $("li a").click(function(){
 	}else{
 		$("#Edit, #Add").hide();
 	};	
+
+	$("#searchBox").hide();
+	AddButtonClickedOn = false;
+	EditButtonClickedOn = false;
+	$(".Delete").hide();
+});
+
+$("#Add","header").click(function(){
+	if(!AddButtonClickedOn){
+		$("#searchBox").show();
+	}else{
+		$("#searchBox").hide();
+	}
+	AddButtonClickedOn = !AddButtonClickedOn;
+});
+
+$("#Edit","header").click(function(){
+	if(!EditButtonClickedOn){
+		$(".Delete").show();
+	}else{
+		$(".Delete").hide();
+	}
+	EditButtonClickedOn = !EditButtonClickedOn;
 });
 
 $(function(){
 	$( "#tabs" ).tabs({
 	});
+	$("#searchBox").hide();
+	$(".Delete").hide();
 });
